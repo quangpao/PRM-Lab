38ad7d330f8a0f7535c91acd47a71e973a3a4aae
quangpao <ddquangbao@gmail.com>
2023-07-07 14:36:54 +0700

    Slot 13 - Content Provider

---
+++ Slot13/ContentProvider/app/src/main/java/com/quangpao/contentprovider/MainActivity.java
@@ -0,0 +1,46 @@
+package com.quangpao.contentprovider;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClickAddName(View view) {
+        ContentValues values = new ContentValues();
+
+        values.put(StudentsProvider.NAME, ((EditText) findViewById(R.id.editText1)).getText().toString());
+        values.put(StudentsProvider.GRADE, ((EditText) findViewById(R.id.editText2)).getText().toString());
+        Uri uri = getContentResolver().insert(StudentsProvider.CONTENT_URI, values);
+        Toast.makeText(getBaseContext(), uri.toString(), Toast.LENGTH_SHORT).show();
+    }
+
+    @SuppressLint("Range")
+    public void onClickRetrieveStudents(View view) {
+        String URL = "content://com.quangpao.contentprovider.StudentsProvider";
+        Uri students = Uri.parse(URL);
+        Cursor c = getContentResolver().query(students, null, null, null, "name");
+        if (c.moveToFirst()) {
+            do {
+                Toast.makeText(this, c.getString(c.getColumnIndex(StudentsProvider._ID))
+                        + ", " +
+                        c.getString(c.getColumnIndex(StudentsProvider.NAME))
+                        + ", " +
+                        c.getString(c.getColumnIndex(StudentsProvider.GRADE)), Toast.LENGTH_SHORT).show();
+            } while (c.moveToNext());
+        }
+    }
+}
